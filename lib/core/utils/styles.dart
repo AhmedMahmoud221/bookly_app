2b8bf96ea579559492de3959bb66f646cc6ea5ac
ahmedmahmoud221 <a.mahmoud2212000@gmail.com>
2025-11-22 17:53:21 +0200

    best seller item part three

--- lib/core/utils/styles.dart
+++ lib/core/utils/styles.dart
@@ -1,4 +1,3 @@
-import 'package:bookly_app/constants.dart';
 import 'package:flutter/material.dart';
 
 abstract class Styles {
@@ -9,12 +8,10 @@ abstract class Styles {
   static const textStyle20 = TextStyle(
     fontSize: 20,
     fontWeight: FontWeight.normal,
-    fontFamily: kGtSectraFine,
   );
   static const textStyle30 = TextStyle(
     fontSize: 30,
     fontWeight: FontWeight.normal,
-    fontFamily: kGtSectraFine,
   );
   static const textStyle14 = TextStyle(
     fontSize: 14,
