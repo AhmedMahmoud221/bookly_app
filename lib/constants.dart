78419f52bd6ef99a0dcd45e0605281902440c218
ahmedmahmoud221 <a.mahmoud2212000@gmail.com>
2025-11-22 19:52:06 +0200

    refactor navigation in our app

--- lib/constants.dart
+++ lib/constants.dart
@@ -2,4 +2,4 @@ import 'package:flutter/widgets.dart';
 
 const kPrimaryColor = Color(0xff100B20);
 const kTranstionDuration = Duration(milliseconds: 250);
-const kGtSectraFine = 'GtSectraFine';
+const kGtSectraFine = 'GT Sectra Fine';
