27d32359b78b9315fa275a28b8fbfe2ee8b8731a
ahmedmahmoud221 <a.mahmoud2212000@gmail.com>
2025-11-22 14:00:10 +0200

    adding sliding animation to splash view

--- lib/Features/Splash/presentation/views/widgets/splash_view_body.dart
+++ lib/Features/Splash/presentation/views/widgets/splash_view_body.dart
@@ -1,20 +1,57 @@
+import 'package:bookly_app/Features/Splash/presentation/views/widgets/sliding_text.dart';
 import 'package:bookly_app/core/utils/assets.dart';
 import 'package:flutter/material.dart';
 
-class SplashViewBody extends StatelessWidget {
+class SplashViewBody extends StatefulWidget {
   const SplashViewBody({super.key});
+
+  @override
+  State<SplashViewBody> createState() => _SplashViewBodyState();
+}
+
+class _SplashViewBodyState extends State<SplashViewBody>
+    with SingleTickerProviderStateMixin {
+  late AnimationController animationController;
+  late Animation<Offset> slidingAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+
+    animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 2),
+    );
+
+    slidingAnimation = Tween<Offset>(
+      begin: const Offset(0, 5),
+      end: Offset.zero,
+    ).animate(animationController);
+
+    animationController.forward();
+    // slidingAnimation.addListener(() {   (we don't need this code becuse used AnimatedBuilder)
+    //   setState(() {});
+    // });
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+
+    animationController.dispose(); //Unsubscribe from streams and close network connections to avoid memory leaks and ensure the application functions efficiently.
+
+  }
 
   @override
   Widget build(BuildContext context) {
     return Column(
       mainAxisAlignment: MainAxisAlignment.center,
       crossAxisAlignment: CrossAxisAlignment.stretch,
-      children: [Image.asset(AssetsData.logo), 
-      const SizedBox(height: 5,),
-      const Text('Read Free Books',
-        textAlign: TextAlign.center,
-      ),
-    ],
+      children: [
+        Image.asset(AssetsData.logo),
+        const SizedBox(height: 5),
+        SlidingText(slidingAnimation: slidingAnimation),
+      ],
     );
   }
 }
